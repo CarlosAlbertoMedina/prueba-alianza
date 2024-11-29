de744a84339096b0643e68effb9b6fcf5d34f896
Carlos Medina <caralb.medina@gmail.com>
2024-11-28 19:00:48 -0500

    Proyecto base

---
+++ src/main/java/com/alianza/demo/PruebaAlianzaApplication.java
@@ -0,0 +1,13 @@
+package com.alianza.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaAlianzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaAlianzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alianza/demo/PruebaAlianzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alianza.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaAlianzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
